04ebba4b6c367e95105de06b2a448ccef606ffe0
tthuma <thumatim@gmail.com>
2021-03-21 22:10:12 +0100

    Fixed accidental change of server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const next = require('next');
 const routes = require('./routes');
-const app = next({ dev: process.env.NODE_ENV !== 'development' });
+const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handler = routes.getRequestHandler(app);
 
 const { createServer } = require('http');
